4d662a05144e764c5a459bae7537a0ba82591a89
Josh <jbarr83@gmail.com>
2023-09-25 08:43:49 -0500

    clean up and comments

--- src/product_update/main.py
+++ src/product_update/main.py
@@ -32,17 +32,23 @@ if __name__ == '__main__':
     with open('../../resources/credentials.yml', 'r') as file:
         credentials = yaml.safe_load(file)
 
-    session = shopify.Session(credentials['sunshine_hut_shopify']['shop_url'], "2023-04",
+    # Set up Shopify session
+    session = shopify.Session(credentials['sunshine_hut_shopify']['shop_url'],
+                              "2023-04",
                               credentials['sunshine_hut_shopify']['admin_token'])
     shopify.ShopifyResource.activate_session(session)
+
+    # Retrieve first page of products
     products: PaginatedCollection[Product] = shopify.Product.find(limit=250)
 
     master_product_list = create_product_list(products)
 
+    # If products have another page of results, retrieve and add to the master list until there are no more pages
     next_page: bool = products.has_next_page()
     while next_page:
         products: PaginatedCollection[Product] = products.next_page()
         master_product_list = [*master_product_list, *create_product_list(products)]
         next_page = products.has_next_page()
 
+    # Output the results to a CSV
     output_to_csv(master_product_list)
